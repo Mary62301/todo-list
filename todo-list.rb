a3f087a766dd48a42cc41d81fa09379aaad648c3
Mary <gorochova.ma@icloud.com>
2025-10-27 12:40:24 +0200

    Add a item to the todo-list

---
+++ todo-list.rb
@@ -0,0 +1,19 @@
+print "Введите название задачи: "
+name = gets.strip
+tasks = ['some task']
+NAME_NAX_SIZE = 10
+
+if name !='' && name.size < NAME_NAX_SIZE
+    tasks << name 
+    tasks.each {|task| puts task}
+else
+    puts "Мы не можем добавить в список задач: "
+    if name == ''
+      puts "Пустое название"
+    end 
+    if name.size > NAME_NAX_SIZE
+      puts "Превышает количество #{NAME_NAX_SIZE} символов"
+    end 
+end 
+
+
