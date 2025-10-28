b2da83c79451a04adc0b6bc48f9bffc6ba998616
Mary <gorochova.ma@icloud.com>
2025-10-28 10:20:40 +0200

    Add min_size to the todo-list

--- todo-list.rb
+++ todo-list.rb
@@ -2,8 +2,9 @@ print "Введите название задачи: "
 name = gets.strip
 tasks = ['some task']
 NAME_NAX_SIZE = 10
+NAME_MIN_SIZE = 3
 
-if name !='' && name.size < NAME_NAX_SIZE
+if name !='' && name.size < NAME_NAX_SIZE && name.size > NAME_MIN_SIZE
     tasks << name 
     tasks.each {|task| puts task}
 else
@@ -14,6 +15,9 @@ else
     if name.size > NAME_NAX_SIZE
       puts "Превышает количество #{NAME_NAX_SIZE} символов"
     end 
+    if name.size < NAME_MIN_SIZE
+      puts "Название содержит меньше #{NAME_MIN_SIZE} символов"
+    end
 end 
 
 
