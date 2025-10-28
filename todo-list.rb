1dd865235018448f2e3d9b26fc6631603b9e306c
Mary <gorochova.ma@icloud.com>
2025-10-28 11:02:14 +0200

    Corrected max_size to the todo-list

--- todo-list.rb
+++ todo-list.rb
@@ -1,10 +1,10 @@
 print "Введите название задачи: "
 name = gets.strip
 tasks = ['some task']
-NAME_NAX_SIZE = 10
+NAME_MAX_SIZE = 10
 NAME_MIN_SIZE = 3
 
-if name !='' && name.size < NAME_NAX_SIZE && name.size > NAME_MIN_SIZE
+if name !='' && name.size < NAME_MAX_SIZE && name.size > NAME_MIN_SIZE
     tasks << name 
     tasks.each {|task| puts task}
 else
@@ -12,8 +12,8 @@ else
     if name == ''
       puts "Пустое название"
     end 
-    if name.size > NAME_NAX_SIZE
-      puts "Превышает количество #{NAME_NAX_SIZE} символов"
+    if name.size > NAME_MAX_SIZE
+      puts "Превышает количество #{NAME_MAX_SIZE} символов"
     end 
     if name.size < NAME_MIN_SIZE
       puts "Название содержит меньше #{NAME_MIN_SIZE} символов"
