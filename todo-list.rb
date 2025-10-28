a907ba121e91a23b372d43229dbd86a5622959da
Mary <gorochova.ma@icloud.com>
2025-10-28 11:30:40 +0200

    Fix name validation

--- todo-list.rb
+++ todo-list.rb
@@ -10,13 +10,13 @@ if name !='' && name.size < NAME_MAX_SIZE && name.size > NAME_MIN_SIZE
 else
     puts "Мы не можем добавить в список задач: "
     if name == ''
-      puts "Пустое название"
+      puts "Пустое название."
     end 
-    if name.size > NAME_MAX_SIZE
-      puts "Превышает количество #{NAME_MAX_SIZE} символов"
+    if name.size >= NAME_MAX_SIZE
+      puts "Название превышает или равен #{NAME_MAX_SIZE} символам."
     end 
-    if name.size < NAME_MIN_SIZE
-      puts "Название содержит меньше #{NAME_MIN_SIZE} символов"
+    if name.size <= NAME_MIN_SIZE
+      puts "Название содержит меньше или равен #{NAME_MIN_SIZE} символам."
     end
 end 
 
